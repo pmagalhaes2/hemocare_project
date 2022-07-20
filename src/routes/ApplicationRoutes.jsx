585fa8ff315a019a17564356cb31fa9e12e6e0af
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-07-19 23:08:23 -0300

    feat: create application routes

---
+++ src/routes/ApplicationRoutes.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "../components/Nav/Nav";
+import Contato from "../pages/Contato/Contato";
+import Home from "../pages/Home/Home";
+import Orientacoes from "../pages/Orientacoes/Orientacoes";
+import Sobre from "../pages/Sobre/Sobre";
+
+function ApplicationRoutes() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/orientacoes" element={<Orientacoes />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default ApplicationRoutes;
