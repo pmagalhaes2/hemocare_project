b8b400e921814ffef5909e8c70567cae2c689e75
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-07-23 00:14:14 -0300

    chore: update home content component

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -1,4 +1,5 @@
 import Button from "../Button/Button";
+import Footer from "../Footer/Footer";
 import "./homecontent.css";
 
 const HomeContent = () => {
@@ -20,10 +21,16 @@ const HomeContent = () => {
             Saiba mais
           </Button>
         </div>
-        <img src="https://github.com/pmagalhaes2/homecare_project/blob/main/src/assets/vector_one.png?raw=true" alt="" />
+        <img
+          src="https://github.com/pmagalhaes2/homecare_project/blob/main/src/assets/vector_one.png?raw=true"
+          alt=""
+        />
       </main>
       <section className="banner">
-        <img src="https://github.com/pmagalhaes2/homecare_project/blob/main/src/assets/vector_two.png?raw=true" alt="" />
+        <img
+          src="https://github.com/pmagalhaes2/homecare_project/blob/main/src/assets/vector_two.png?raw=true"
+          alt=""
+        />
         <div className="text-container">
           <h2 className="subtitle-white">
             14/6 – Dia Mundial do Doador de Sangue
@@ -43,26 +50,45 @@ const HomeContent = () => {
         <div className="donation-container">
           <ul className="donation-informations">
             <li className="subtitle-black">Doações no Brasil:</li>
+            <div className="percentage">
+              <span className="line red"></span>
+              <span>1,9%</span>
+            </div>
             <p className="text-grey">
               De acordo com dados do Ministério da Saúde (MS), 16 a cada mil
               habitantes são doadores de sangue no país, o que corresponde a
               1,9% da população brasileira.
             </p>
+
             <li className="subtitle-black">Mínimo recomendado:</li>
+            <div className="percentage">
+              <span className="line medium-red"></span>
+              <span>3%</span>
+            </div>
             <p className="text-grey">
               Um país deve ter um mínimo de 3% de doadores em relação ao total
               da população, segundo recomendação da Organização Mundial da Saúde
               (OMS).
             </p>
+
             <li className="subtitle-black">Quantidade ideal:</li>
+            <div className="percentage">
+              <span className="line soft-red"></span>
+              <span>5%</span>
+            </div>
             <p className="text-grey">
               A recomendação (OMS), é que seja de pelo menos 5% em relação a
               população total.
             </p>
           </ul>
-          <img src="https://github.com/pmagalhaes2/homecare_project/blob/main/src/assets/vector_three.png?raw=true" alt="" />
+          <img
+            className="donation-vector"
+            src="https://github.com/pmagalhaes2/homecare_project/blob/main/src/assets/vector_three.png?raw=true"
+            alt=""
+          />
         </div>
       </section>
+      <Footer />
     </>
   );
 };
