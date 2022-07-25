a5649a2c7798773afff521f0c51aa68bde98e677
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-07-24 23:20:52 -0300

    chore: update footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,26 +4,40 @@ import {
   AiOutlineMail,
 } from "react-icons/ai";
 
+import { AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+
 import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <ul>
-        <li>
-          <AiOutlinePhone />
-          <p>(11) 9000-0000</p>
-        </li>
-        <li>
-          <AiOutlineWhatsApp />
-          <p>(11) 9000-0000</p>
-        </li>
-        <li>
-          <AiOutlineMail />
-          <p>aloatendimento@homecare.com.br</p>
-        </li>
-      </ul>
-    </footer>
+    <>
+      <footer>
+        <div className="contact-infos">
+          <ul>
+            <li>
+              <AiOutlinePhone />
+              <p>(11) 9000-0000</p>
+            </li>
+            <li>
+              <AiOutlineWhatsApp />
+              <p>(11) 9000-0000</p>
+            </li>
+            <li>
+              <AiOutlineMail />
+              <p>aloatendimento@homecare.com.br</p>
+            </li>
+          </ul>
+        </div>
+        <div className="icons">
+          <AiFillFacebook />
+          <AiFillInstagram />
+          <img src="/src/assets/logo-hc 2.png" alt="" />
+        </div>
+      </footer>
+      <div className="copyright">
+        <p>Copyright 2022 © Todos os direitos reservados</p>
+      </div>
+    </>
   );
 };
 
